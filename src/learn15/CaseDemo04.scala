6820f4f30449fec6df790dc2f6c524d1dfa70d0d
BUPTAnderson <229546106@qq.com>
2017-09-02 22:54:52 +0800

    update

---
+++ src/learn15/CaseDemo04.scala
@@ -0,0 +1,16 @@
+package learn15
+
+/**
+ * Created by kongyunlong on 2017/9/2.
+ */
+object CaseDemo04 {
+  def test (name : String, age : Int): Unit = {
+    val sql = s"select * from student where name='${name}' and age >= ${age}"
+    val s =
+    """
+      select * from
+      student
+      where
+    """
+  }
+}
